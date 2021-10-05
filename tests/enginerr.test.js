ca640819454d0b493cdd66eb94cd56ad642f270e
JaredAcker <jaredacker1@gmail.com>
2021-10-05 18:09:33 -0400

    Update enginerr.test.js
    changed the engineer test file

--- tests/enginerr.test.js
+++ tests/enginerr.test.js
@@ -1,14 +1,28 @@
-const { test, expect } = require('@jest/globals');
-const Engineer = require('../lib/engineer');
+const Engineer = require('../lib/engineer')
 
-test('Retrieve the github account', () => {
-    const testGithub = 'testUser';
-    const testEngineer = new Engineer('test', 2, 'test@test.com', testGithub);
-    expect(testEngineer.github).toBe(testGithub);
-});
+describe("Engineer", () => {
+    it('Create Engineer object', () => {
+        const engineer = new Engineer('Jared', '6882', 'jaredacker1@gmail.com');
 
-test('The role will read Engineer', () => {
-    const testRole = 'Engineer';
-    const testEngineer = new Engineer('test', 2, 'test@test.com', 'testUser');
-    expect(testEngineer.getRole()).toBe(testRole);
+        expect(engineer.name).toBe('Jared');
+        expect(engineer.id).toBe('6882');
+        expect(engineer.email).toBe('jaredacker1@gmail.com');
+        expect(engineer.type).toBe('Engineer');
+    });
+    it('Output engineer name', () =>{
+        const engineer = new Engineer('Jared', 'jaredacker1@gmail.com');
+        expect(engineer.getName()).toBe('Jared');
+    });
+    it('Output engineer ID', () =>{
+        const engineer = new Engineer('Jared', 'jaredacker1@gmail.com');
+        expect(engineer.getId()).toBe('6882');
+    });
+    it('Output engineer email', () =>{
+        const engineer = new Engineer('Jared', 'jaredacker1@gmail.com');
+        expect(engineer.getEmail()).toBe('jaredacker1@gmail.com');
+    });
+    it('Output engineer Role', () =>{
+        const engineer = new Engineer('Jared', 'jaredacker1@gmail.com');
+        expect(engineer.getRole()).toBe('Engineer');
+    });
 });

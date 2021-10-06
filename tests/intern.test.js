5453b3acecef51994dcb68d70936d7dc00b1aab6
JaredAcker <jaredacker1@gmail.com>
2021-10-05 20:54:24 -0400

    Update intern.test.js
    added more information to what these it statements should be calling for

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -7,22 +7,27 @@ describe("Intern", () => {
         expect(intern.name).toBe('Jared');
         expect(intern.id).toBe('6882');
         expect(intern.email).toBe('jaredacker1@gmail.com');
-        expect(intern.type).toBe('Intern');
+        expect(intern.school).toBe('OSU');
+        expect(intern.child).toBe('Intern');
     });
     it('Output intern name', () =>{
-        const intern = new Intern('Jared', 'jaredacker1@gmail.com');
+        const intern = new Intern('Jared', '6882', 'jaredacker1@gmail.com', 'OSU', 'Intern');
         expect(intern.getName()).toBe('Jared');
     });
     it('Output intern ID', () =>{
-        const intern = new Intern('Jared', 'jaredacker1@gmail.com');
+        const intern = new Intern('Jared', '6882', 'jaredacker1@gmail.com', 'OSU', 'Intern');
         expect(intern.getId()).toBe('6882');
     });
     it('Output intern email', () =>{
-        const intern = new Intern('Jared', 'jaredacker1@gmail.com');
+        const intern = new Intern('Jared', '6882', 'jaredacker1@gmail.com', 'OSU', 'Intern');
         expect(intern.getEmail()).toBe('jaredacker1@gmail.com');
     });
     it('Output intern Role', () =>{
-        const intern = new Intern('Jared', 'jaredacker1@gmail.com');
-        expect(intern.getRole()).toBe('Intern');
+        const intern = new Intern('Jared', '6882', 'jaredacker1@gmail.com', 'OSU', 'Intern');
+        expect(intern.getChild()).toBe('Intern');
+    });
+    it('Output intern school', () =>{
+        const intern = new Intern('Jared', '6882', 'jaredacker1@gmail.com', 'OSU', 'Intern');
+        expect(intern.getsSchool()).toBe('OSU');
     });
 });

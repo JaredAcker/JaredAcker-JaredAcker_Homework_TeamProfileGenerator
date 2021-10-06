e95e1a07f448bc0b5ba3fe4d3b31788b065a5f62
JaredAcker <jaredacker1@gmail.com>
2021-10-05 20:53:53 -0400

    Update employee.test.js
    added more information to what these it statements should be calling for

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require('../lib/Employee')
+const Employee = require('../lib/employee')
 
 describe("Employee", () => {
     it('Create Employee object', () => {
@@ -7,22 +7,22 @@ describe("Employee", () => {
         expect(employee.name).toBe('Jared');
         expect(employee.id).toBe('6882');
         expect(employee.email).toBe('jaredacker1@gmail.com');
-        expect(employee.type).toBe('Employee');
+        expect(employee.role).toBe('Employee');
     });
     it('Output employee name', () =>{
-        const employee = new Employee('Jared', 'jaredacker1@gmail.com');
+        const employee = new Employee('Jared', '6882', 'jaredacker1@gmail.com', 'Employee');
         expect(employee.getName()).toBe('Jared');
     });
     it('Output employee ID', () =>{
-        const employee = new Employee('Jared', 'jaredacker1@gmail.com');
+        const employee = new Employee('Jared', '6882', 'jaredacker1@gmail.com', 'Employee');
         expect(employee.getId()).toBe('6882');
     });
     it('Output employee email', () =>{
-        const employee = new Employee('Jared', 'jaredacker1@gmail.com');
+        const employee = new Employee('Jared', '6882', 'jaredacker1@gmail.com', 'Employee');
         expect(employee.getEmail()).toBe('jaredacker1@gmail.com');
     });
-    it('Output employee Role', () =>{
-        const employee = new Employee('Jared', 'jaredacker1@gmail.com');
+    it('Output employee role', () =>{
+        const employee = new Employee('Jared', '6882', 'jaredacker1@gmail.com', 'Employee');
         expect(employee.getRole()).toBe('Employee');
     });
 });
